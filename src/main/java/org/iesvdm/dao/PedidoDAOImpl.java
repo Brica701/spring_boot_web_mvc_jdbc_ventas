08bdecf23a6434ca93770e6afc59f2d9cebad4a6
Isaac <icrupin2204@g.educaand.es>
2025-01-27 11:01:53 +0100

    ...

--- src/main/java/org/iesvdm/dao/PedidoDAOImpl.java
+++ src/main/java/org/iesvdm/dao/PedidoDAOImpl.java
@@ -1,5 +1,6 @@
 package org.iesvdm.dao;
 
+import ch.qos.logback.classic.Logger;
 import lombok.extern.slf4j.Slf4j;
 import org.iesvdm.modelo.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
